270c52d544be6b1e97b0e4649dad5fa32b7d8371
Nathan Cahill <nathan@nathancahill.com>
2014-08-04 13:26:34 -0600

    packaged for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-Logging',
+    version='0.1.2',
+    packages=['flask_logging',],
+    license='MIT',
+    url='https://pypi.python.org/pypi/Flask-Logging',
+    author='Nathan Cahill',
+    author_email='nathan@nathancahill.com',
+    long_description=open('README').read(),
+)
